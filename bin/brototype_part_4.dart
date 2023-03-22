92d4189ae1809f0272a0a08f1c67402f04739a0f
yahiyam~ <yahiyamuhammed00@gmail.com>
2023-03-22 13:05:51 +0530

    list explanation

---
+++ bin/brototype_part_4.dart
@@ -0,0 +1,19 @@
+//[ 20, 35, 40, 10, 22, 32, 19]
+/* 
+init 
+set       add value by replacing another value
+get
+traverse
+insert    add new space and value
+delete
+ */
+//declaration and initialisation
+void main(List<String> args) {
+  List list; //declaration
+  list = [1, 2]; //initialisatoin  ==> O(n)ST
+  print(list);
+  list[0] = 2;   //set             ==> O(1)ST 
+  print(list);
+}
+//dynamic list && static list
+//dynamic list    worst case ==> O(n)ST   best case ==> O(1)ST
