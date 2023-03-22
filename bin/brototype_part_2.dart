8986dd5ffa165f7085888a04e19acc59a6ffdd68
yahiyam~ <yahiyamuhammed00@gmail.com>
2023-03-22 10:18:28 +0530

    notes from brototype

---
+++ bin/brototype_part_2.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  
+}
+//ds = organisation of data, eg:stack,queue
+//algorithm = step by step instructions of code
+//complexity analysis = analysis the soln and make better on the basis of TCA and SPA
+//Big O notation
