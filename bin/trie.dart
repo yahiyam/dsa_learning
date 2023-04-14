19da5cb9af15d4c3a90ed02439bae631b6471b31
yahiyam~ <yahiyamuhammed00@gmail.com>
2023-04-14 12:16:13 +0530

    trie first commit

--- bin/trie.dart
+++ bin/trie.dart
@@ -1,3 +1,64 @@
-class ClassName {
-  
+class TrieNode {
+  Map<String, TrieNode> children = {};
+
+  void addChild(String letter, TrieNode node) {
+    children[letter] = node;
+  }
+
+  bool containsChild(String letter) {
+    return children.containsKey(letter);
+  }
+
+  TrieNode? getChild(String letter) {
+    return children[letter];
+  }
+
+  bool containsEndSymbol() {
+    return children.containsKey('*');
+  }
+}
+
+class Trie {
+  TrieNode root = TrieNode();
+  String endSymbol = '*';
+
+  Trie(String str) {
+    populateSuffixTrie(str);
+  }
+
+  void populateSuffixTrie(String str) {
+    for (int i = 0; i < str.length; i++) {
+      insertSubstringStartingAt(i, str);
+    }
+  }
+
+  void insertSubstringStartingAt(int index, String str) {
+    TrieNode node = root;
+    for (int i = index; i < str.length; i++) {
+      String letter = str[i];
+      if (!node.containsChild(letter)) {
+        TrieNode newNode = TrieNode();
+        node.addChild(letter, newNode);
+      }
+      node = node.getChild(letter)!;
+    }
+    node.addChild(endSymbol, null);
+  }
+
+  bool contains(String str) {
+    TrieNode node = root;
+    for (int i = 0; i < str.length; i++) {
+      String letter = str[i];
+      if (!node.containsChild(letter)) {
+        return false;
+      }
+      node = node.getChild(letter)!;
+    }
+    return node.containsEndSymbol();
+  }
+}
+
+void main() {
+  Trie trie = Trie('banana');
+  print(trie.contains('nan')); // output: true
 }
